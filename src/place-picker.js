b6baa31bc07b994de8bec7c03598efa92bcbf436
Francois Wouts <francois.wouts@gmail.com>
2015-12-06 12:08:36 +1100

    Update the sample to match the blog post.

--- src/place-picker.js
+++ src/place-picker.js
@@ -2,6 +2,7 @@ import {inject, customElement, bindable} from 'aurelia-framework';
 import mapsapi from 'google-maps-api';
 
 @customElement('place-picker')
+// Get an API key from https://developers.google.com/maps/documentation/javascript/get-api-key.
 @inject(Element, mapsapi('YOUR_API_KEY_HERE', ['places']))
 export class PlacePicker {
 
@@ -10,27 +11,31 @@ export class PlacePicker {
   constructor(element, mapsApi) {
     this.element = element;
     this.mapsApi = mapsApi;
-    this.maps = null;
   }
 
   attached() {
+    // This loads the Google Maps API asynchronously.
     this.mapsApi.then(maps => {
-      this.maps = maps;
+      // Now that it's loaded, add a map to our HTML.
       var mapContainer = this.element.querySelector('.place-picker-map');
-      var map = new this.maps.Map(mapContainer, {
+      var map = new maps.Map(mapContainer, {
         center: {lat: -33.8688, lng: 151.2195},
         zoom: 13
       });
+
+      // Also convert our input field into a place autocomplete field.
       var input = this.element.querySelector('input');
       var autocomplete = new google.maps.places.Autocomplete(input);
       map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
       autocomplete.bindTo('bounds', map);
 
+      // Create a marker that will show where the selected place is.
       var marker = new google.maps.Marker({
         map: map,
         anchorPoint: new google.maps.Point(0, -29)
       });
 
+      // Create a lambda that moves the marker and the map viewport.
       let updateMarker = () => {
         var position = new google.maps.LatLng(this.location.lat, this.location.lng);
         map.setCenter(position);
@@ -38,7 +43,10 @@ export class PlacePicker {
         marker.setVisible(true);
       };
 
+      // Ensure that the current location is shown properly.
       updateMarker();
+
+      // Update the location and its marker every time a new place is selected.
       autocomplete.addListener('place_changed', () => {
         marker.setVisible(false);
         var place = autocomplete.getPlace();
